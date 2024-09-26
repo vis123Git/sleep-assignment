4dc40151b8472f52da1658d6ec0856260b95b9be
VishalKumarSain <vishaldhoria007@gmail.com>
2024-09-26 19:55:39 +0530

    auth

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("dotenv").config();
 const cors = require("cors");
-const swaggerDocs = require('./swagger')
+const swaggerDocs = require("./swagger");
 const indexRouter = require("./routes/index.js");
 const usersRouter = require("./routes/users.js");
 const { db_connection } = require("./configs/connect_db.js");
@@ -13,7 +13,7 @@ const { db_connection } = require("./configs/connect_db.js");
 const app = express();
 swaggerDocs(app);
 
-db_connection()
+db_connection();
 app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -25,17 +25,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-const whitelist = process.env.WHITELIST;
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name, user-access-token, authorization");
+  res.header("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name, user-access-token, authorization");
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+
+  const origin = req.header("Origin") ? req.header("Origin").replace(/\/$/, "") : null;
+  const referer = req.header("Referer") ? new URL(req.header("Referer")).origin.replace(/\/$/, "") : null;
+
+  // Bypass Origin/Referer check for local development or same-origin requests
+  if (!origin && !referer && process.env.NODE_ENV === "development") {
+    return next();
+  }
+
+  // Handle OPTIONS preflight requests
   if (req.method === "OPTIONS") {
     return res.status(200).json({});
-  } else if (whitelist.indexOf(req.header("Origin")) !== -1 || whitelist.indexOf(req.header("Referer")) !== -1) {
-    next();
+  }
+
+  // Normalize the origin/referer comparison to avoid trailing slash issues
+  if ((origin && whitelist.includes(origin)) || (referer && whitelist.includes(referer))) {
+    return next();
   } else {
     return res.status(400).json("App UnAuthorized");
   }
@@ -54,7 +65,7 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-  
+
   // render the error page
   res.status(err.status || 500);
   res.render("error");
